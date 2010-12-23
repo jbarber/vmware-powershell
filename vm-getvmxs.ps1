8d55b05382494d8021c9ee54a536b25a1d336bb8
Jonathan Barber <jonathan.barber@gmail.com>
2010-12-23 09:25:03 +0000

    Script to retrive all VM .vmx configuration files

---
+++ vm-getvmxs.ps1
@@ -0,0 +1,40 @@
+# Find all the VMX files on all of the Datastores and download them
+
+function VmGetVMXs {
+  # First find the .vmx files
+  $fileQueryFlags = new-object VMware.Vim.FileQueryFlags -property @{ FileSize = $true; FileType = $true; Modification = $true }
+  $searchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec -property @{ details = $fileQueryFlags; sortFoldersFirst = $true; matchPattern = "*.vmx" }
+  
+  $files = Get-Datastore | %{
+    $ds_store = $_
+    $ds = Get-View -viobject $_
+    $res = @()
+    $dsBrowser = Get-View $ds.browser
+  
+    $rootPath = "["+$ds.summary.Name+"]"
+    $searchResult = $dsBrowser.SearchDatastoreSubFolders($rootPath, $searchSpec)
+    foreach ($folder in $searchResult) {
+      foreach ($fileResult in $folder.File) {
+        if ($fileResult.Path -eq $null) { continue } 
+        $file = "" | select Name, Size, Modified, FullPath, DataStore
+        $file.DataStore = $ds_store
+        $file.Name = $fileResult.Path
+        $file.Size = $fileResult.Filesize
+        $file.Modified = $fileResult.Modification
+        $file.FullPath = $folder.FolderPath + $file.Name
+        $res += $file
+      }
+    }
+  
+    $res
+  }
+
+  # This is the bit that actually does the copying...
+  foreach ($file in $files) {
+    New-PSDrive -Location $ds_store -Name source -PSProvider VimDatastore -Root '\'
+    foreach ($vmx in $res) {
+      Copy-DatastoreItem ("source:/", ($vmx.fullpath -split " ")[1] -join "") -Destination ./
+    }
+    Remove-PSDrive source
+  }
+}
